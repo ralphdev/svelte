485e8f31747c2fc9d1f083d598547a57100c4d18
Rafael Martinez <rafa.developers@gmail.com>
2021-07-22 15:40:36 -0500

    Endpoints

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,23 @@
+<script context="module">
+    export const load = async ({ fetch }) => {
+        const res =  await fetch('/api/posts');
+        const data = await res.json();
+
+        console.log(data);
+
+        return {
+            props: {
+                data,
+            }
+        };
+    };
+</script>
+
+<script>
+    export let data;
+</script>
+
 <h1>Homepage</h1>
+<pre>
+    {JSON.stringify(data)}
+</pre>

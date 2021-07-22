732facc96930888425b7d33b7eeabf7b22160333
Rafael Martinez <rafa.developers@gmail.com>
2021-07-22 12:32:33 -0500

    Intro, Pages, Layouts & Components

---
+++ src/lib/nav.svelte
@@ -0,0 +1,16 @@
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+    </ul>
+</nav>
+<style>
+    ul {
+        display: flex;
+        list-style: none;
+    }
+    li {
+        margin-right: 10px;
+    }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,6 @@
+<script>
+    import Nav from '$lib/nav.svelte';
+</script>
+
+<Nav></Nav>
+<slot></slot>
